10dbcc12e745989a22f22bd289a4c1ee83bff96f
Andrew Burgess <andrew@deceptacle.com>
2016-03-25 10:23:21 -0700

    feat: add initial eslint configuration

---
+++ index.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "ecmaFeatures": {
+        "modules": true
+    },
+    "env": {
+        "browser": true,
+        "es6": true,
+        "mocha": true,
+        "node": true
+    },
+    "parser": "babel-eslint",
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "consistent-this": [2, "self"],
+        "no-console": 1,
+        "no-debugger": 1,
+        "no-multiple-empty-lines": 1,
+        "react/jsx-boolean-value": 1,
+        "react/jsx-no-undef": 1,
+        "react/jsx-sort-props": 1,
+        "react/jsx-sort-prop-types": 1,
+        "react/jsx-uses-react": 1,
+        "react/jsx-uses-vars": 1,
+        "react/no-did-mount-set-state": 1,
+        "react/no-did-update-set-state": 1,
+        "react/no-multi-comp": 1,
+        "react/no-unknown-property": 1,
+        "react/prop-types": 1,
+        "react/react-in-jsx-scope": 1,
+        "react/self-closing-comp": 1,
+        "react/wrap-multilines": 1,
+    }
+};
