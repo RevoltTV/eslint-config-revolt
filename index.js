0307f47c80548b2cb67fcd1c411e0d1fed4cd17d
Andrew Burgess <andrew@andrewburgess.io>
2016-11-03 12:01:04 -0700

    :wrench: chore: update dependencies and linter configuration

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 module.exports = {
-    "extends": "eslint:recommended",
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
     "env": {
         "browser": true,
         "es6": true,
@@ -10,9 +13,11 @@ module.exports = {
     "parserOptions": {
         "ecmaFeatures": {
             "experimentalObjectRestSpread": true,
-            "jsx": true
+            "impliedStrict": true,
+            "jsx": true,
+            "modules": true
         },
-        "ecmaVersion": 6,
+        "ecmaVersion": 7,
         "sourceType": "module"
     },
     "plugins": [
@@ -20,23 +25,10 @@ module.exports = {
     ],
     "rules": {
         "consistent-this": [2, "self"],
+        "indent": ["error", 4, { "SwitchCase": 1 }],
         "no-console": 1,
-        "no-debugger": 1,
-        "no-multiple-empty-lines": 1,
-        "react/jsx-boolean-value": 1,
-        "react/jsx-no-undef": 1,
-        "react/jsx-sort-props": 1,
-        "react/jsx-uses-react": 1,
-        "react/jsx-uses-vars": 1,
-        "react/jsx-wrap-multilines": 1,
-        "react/no-did-mount-set-state": 1,
-        "react/no-did-update-set-state": 1,
-        "react/no-multi-comp": 1,
-        "react/no-unknown-property": 1,
-        "react/prop-types": 1,
-        "react/react-in-jsx-scope": 1,
-        "react/self-closing-comp": 1,
-        "react/sort-prop-types": 0,
+        "no-debugger": 2,
+        "no-multiple-empty-lines": 2,
         "semi": ["error", "always"]
     }
 };
